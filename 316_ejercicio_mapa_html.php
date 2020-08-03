148c017f580574cd46dcf7c4bce55917345fc34c
ditasmer <ditasmer@gmail.com>
2020-08-03 13:02:29 +0200

    Ficheros

---
+++ 316_ejercicio_mapa_html.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<html>
+<head>
+	<title></title>
+	<style>
+		div {width:725px; height:483px; background: url(img/Bluemap.jpg); margin:auto; overflow:hidden; position:relative}
+		
+		img {width:50px;position:absolute;}
+	</style>
+</head>
+<body>
+	<div>
+		
+	</div>
+</body>
+</html>
